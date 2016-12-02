ade15179b69542e1fee7ddce9a03f9f63c93676b
witmicko <mogrodni@redhat.com>
2016-12-02 14:59:21 +0000

    sync up with Tom's changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,24 @@
+'use strict';
+
 module.exports = function(grunt) {
-  'use strict';
+  require('load-grunt-tasks')(grunt);
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     eslint: {
       src: ["lib/**/*.js"]
+    },
+    shell: {
+      unit: {
+        options: {
+          stdout: true,
+          stderr: true
+        },
+        command: 'env NODE_PATH=. ./node_modules/.bin/mocha -A -u exports --recursive **/*-spec.js'
+      }
     }
   });
+
   grunt.loadNpmTasks("grunt-eslint");
   grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('unit', ['eslint','shell:unit']);
 };
